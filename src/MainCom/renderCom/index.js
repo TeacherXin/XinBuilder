28b542453d1978e759b46bff7724f42b5fb65d7b
xuhexin <xuhxin@yonyou.com>
2023-03-15 20:03:36 +0800

    fix: 画布区拖拽节点，确定准备位置

--- src/MainCom/renderCom/index.js
+++ src/MainCom/renderCom/index.js
@@ -5,16 +5,31 @@ export default function RenderCom(props) {
 
   //用来存储画布区的所有组件，以及组件的属性
   const [comList, setComList] = useState([])
+
+  let startLeft,startTop,endLeft,endTop,itemLeft,itemTop;
   //用来判断是左侧列表拖拽还是画布区拖拽
   let dragCom = null;
   const {NowCom} = props
 
   const onDrop = (e) => {
     //用来确定拖拽的节点的位置
-    const style = {
-      position: 'absolute',
-      left: e.clientX + 'px',
-      top: e.clientY + 'px',
+
+    endLeft = e.clientX;
+    endTop = e.clientY;
+
+    let style;
+    if(startLeft && startTop){
+      style = {
+        position: 'absolute',
+        left: (endLeft - startLeft) + itemLeft + 'px',
+        top: (endTop - startTop) + itemTop + 'px',
+      }
+    }else{
+      style = {
+        position: 'absolute',
+        left: e.clientX + 'px',
+        top: e.clientY + 'px',
+      }
     }
     setComList([...comList,{component: dragCom || NowCom.component, style,dragId: NowCom.name + e.clientX}])
   }
@@ -26,7 +41,11 @@ export default function RenderCom(props) {
       const component = comList.splice(index,1)[0].component;
       dragCom = component;
     }
-    setComList(comList)
+    setComList(comList);
+    startLeft = e.clientX;
+    startTop = e.clientY;
+    itemLeft = e.target.offsetLeft;
+    itemTop = e.target.offsetTop;
   }
 
   const onDragEnter = (e) => {
