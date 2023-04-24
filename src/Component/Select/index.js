9d0e545ce725fa04fe94350592ce8650507c23e2
xuhexin <xuhxin@yonyou.com>
2023-04-24 15:49:07 +0800

    fix: 增加select组件

---
+++ src/Component/Select/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+export default function Select() {
+  return (
+    <div className='componentSelect'>
+      <select disabled>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+      </select>
+    </div>
+  )
+}
--- src/Component/index.js
+++ src/Component/index.js
@@ -2,10 +2,12 @@ import Button from './Button'
 import Input from './Input'
 import Label from './Label'
 import Table from './Table'
+import Select from './Select'
 
 export {
   Button,
   Input,
   Label,
+  Select,
   Table
 }
