ad7c4b68f247d8e4dea6670077aab1bd5605908a
xuhexin <xuhxin@yonyou.com>
2023-04-26 09:52:44 +0800

    fix: 给链接添加动作和样式配置

--- src/Component/Link/index.js
+++ src/Component/Link/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { useEffect } from 'react';
 
 export default function Link(props) {
 
-  const {renderFlag, onContextMenu, attributeValue, url, openType} = props
+  const {renderFlag, onContextMenu, attributeValue, url, openType, actionJs, styleCss} = props
+
+  useEffect(() => {
+    let css = document.createElement('style');
+    css.innerHTML = styleCss;
+    document.body.append(css)
+  },[styleCss])
+
+  const aclick = () => {
+    let script = document.createElement('script');
+    script.innerHTML = '(function(){' +  actionJs?.click + '})()'
+    document.body.append(script)
+  }
 
   return (
     <div>
-      <div onContextMenu={onContextMenu}>
+      <div onClick={aclick} onContextMenu={onContextMenu}>
         {renderFlag? <a target={openType ? '_blank' : ''} href={url || 'https://www.baidu.com'}>{attributeValue || '链接'}</a> : <span>{attributeValue || '链接'}</span>}
       </div>
     </div>
