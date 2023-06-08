72d82cecf776fc8ec8a97bf7b6d5be16c6c0ae86
xuhexin <xuhxin@yonyou.com>
2023-06-08 20:22:20 +0800

    fix: 路由懒加载

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React,{lazy,Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import MetaRender from './MetaRender';
-import PageList from './PageList';
 import { HashRouter as Router, Routes , Route} from "react-router-dom";
+
+const PageList = lazy(() => import('./PageList'))
+const App = lazy(() => import('./App'))
+const MetaRender = lazy(() => import('./MetaRender'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
+    <Suspense>
     <Routes>
       <Route path={'/'} element={<PageList />}></Route>
       <Route path={'/home'} element={<App />}></Route>
       <Route path={'/metaRender'} element={<MetaRender />}></Route>
     </Routes>
+    </Suspense>
   </Router>
 );
